9c4fc2e5fdd75d92b12cb6d899527dfe16a6bb6d
AA <almazg@Mac-mini-Almaz.local>
2020-04-17 15:51:06 +0300

    c_data

--- srcs/c_data.c
+++ srcs/c_data.c
@@ -1,4 +1,26 @@
-//
-// Created by Алмаз on 17.04.2020.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   c_data.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almazg <almazg@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/17 11:58:12 by almazg            #+#    #+#             */
+/*   Updated: 2020/04/17 12:08:05 by almazg           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "../includes/asm.h"
+
+void			c_data(t_file *ssl, t_exec *com, t_label *list)
+{
+	label_change(list, &com);
+	free_label(list);
+	arg_add(&com);
+	if ((ssl->text = open(ssl->f_name, O_CREAT | O_TRUNC | O_WRONLY,
+						  0644)) == -1)
+		write_error("FILE DOESN'T CREAT");
+	including_magic(ssl->text);
+	including_size(ssl);
+	line_to_write(&com, ssl);
+}
