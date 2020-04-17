f96eabdac9689f52a535d6c2d3a193275ce2156f
AA <almazg@Mac-mini-Almaz.local>
2020-04-17 15:47:27 +0300

    com_name

--- srcs/name_comment.c
+++ srcs/name_comment.c
@@ -6,7 +6,7 @@
 /*   By: almazg <almazg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/13 18:00:34 by almazg            #+#    #+#             */
-/*   Updated: 2020/04/14 17:17:56 by almazg           ###   ########.fr       */
+/*   Updated: 2020/04/17 11:48:21 by almazg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,28 +22,29 @@ void	name_comment(char *line, t_file *ssl)
 	while (!(ssl->n && ssl->c) && (get_next_line(ssl->fd, &line)) > 0)
 	{
 		ssl->a = 0;
-		while (line[ssl->a])
-		{
-			skip_probel(ssl, line);
-			no_comment(ssl, line);
-			ft_strncpy(n, &line[ssl->a], 5);
-			ft_strncpy(c, &line[ssl->a], 8);
-			if (line[ssl->a])
-			{
-				if (ft_strcmp(n, ".name") == 0 && !ssl->n)
-				{
-					check_name(line, ssl);
-					break ;
-				}
-				else if (ft_strcmp(c, ".comment") == 0 && !ssl->c)
-				{
-					check_comment(line, ssl);
-					break ;
-				}
-				else
-					write_error("COMMENT AND NAME ERROR");
-			}
-		}
+		choose_n_c(line, ssl, n,c);
+//		while (line[ssl->a])
+//		{
+//			skip_probel(ssl, line);
+//			no_comment(ssl, line);
+//			ft_strncpy(n, &line[ssl->a], 5);
+//			ft_strncpy(c, &line[ssl->a], 8);
+//			if (line[ssl->a])
+//			{
+//				if (ft_strcmp(n, ".name") == 0 && !ssl->n)
+//				{
+//					check_name(line, ssl);
+//					break ;
+//				}
+//				else if (ft_strcmp(c, ".comment") == 0 && !ssl->c)
+//				{
+//					check_comment(line, ssl);
+//					break ;
+//				}
+//				else
+//					write_error("COMMENT AND NAME ERROR");
+//			}
+//		}
 		ft_strdel(&line);
 	}
 	ft_memdel((void**)&n);
