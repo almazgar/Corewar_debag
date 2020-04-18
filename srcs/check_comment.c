bb121e63a915c89b112b6ebedace5619822525ab
Almaz <62278408+almazgar@users.noreply.github.com>
2020-04-18 13:10:46 +0300

    Add files via upload

--- srcs/check_comment.c
+++ srcs/check_comment.c
@@ -6,7 +6,7 @@
 /*   By: lgarse <lgarse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/03 12:52:44 by almazg            #+#    #+#             */
-/*   Updated: 2020/04/14 17:17:56 by almazg           ###   ########.fr       */
+/*   Updated: 2020/04/18 13:03:49 by almazg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,34 +23,36 @@ void		check_comment(char *line, t_file *ssl)
 	while (line[ssl->a] && is_whitespace(line[ssl->a]))
 		(ssl->a)++;
 	if (line[ssl->a] != '"')
-	{
-		while (line[ssl->a] != '"' && (get_next_line(ssl->fd, &line)) > 0)
-		{
-			ssl->a = 0;
-			while (line[ssl->a] && is_whitespace(line[ssl->a]))
-				(ssl->a)++;
-			if (line[ssl->a] && line[ssl->a] != '"')
-				write_error("ERROR COMMENT");
-			ft_strdel(&line);
-		}
-	}
+		check_comment2(line, ssl);
+//	{
+//		while (line[ssl->a] != '"' && (get_next_line(ssl->fd, &line)) > 0)
+//		{
+//			ssl->a = 0;
+//			while (line[ssl->a] && is_whitespace(line[ssl->a]))
+//				(ssl->a)++;
+//			if (line[ssl->a] && line[ssl->a] != '"')
+//				write_error("ERROR COMMENT");
+//			ft_strdel(&line);
+//		}
+//	}
 	(ssl->a)++;
 	while (line[ssl->a] && line[ssl->a] != '"')
 		s[j++] = line[(ssl->a)++];
 	if (line[ssl->a] != '"')
-	{
-		s[j++] = '\n';
-		while ((get_next_line(ssl->fd, &line)) > 0)
-		{
-			ssl->a = 0;
-			while (line[ssl->a] && line[ssl->a] != '"')
-				s[j++] = line[(ssl->a)++];
-			if (line[ssl->a] == '"')
-				break ;
-			s[j++] = '\n';
-			ft_strdel(&line);
-		}
-	}
+		j = check_comment3(line, ssl, j, s);
+//	{
+//		s[j++] = '\n';
+//		while ((get_next_line(ssl->fd, &line)) > 0)
+//		{
+//			ssl->a = 0;
+//			while (line[ssl->a] && line[ssl->a] != '"')
+//				s[j++] = line[(ssl->a)++];
+//			if (line[ssl->a] == '"')
+//				break ;
+//			s[j++] = '\n';
+//			ft_strdel(&line);
+//		}
+//	}
 	if (line[ssl->a] != '"')
 		write_error("ERROR NO FINISH COMMENT");
 	if (j > COMMENT_LENGTH)
