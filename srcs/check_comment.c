1244a1785a5dd1240b226e29653229b3d7260573
Almaz <62278408+almazgar@users.noreply.github.com>
2020-04-18 13:22:28 +0300

    Add files via upload

--- srcs/check_comment.c
+++ srcs/check_comment.c
@@ -24,35 +24,11 @@ void		check_comment(char *line, t_file *ssl)
 		(ssl->a)++;
 	if (line[ssl->a] != '"')
 		check_comment2(line, ssl);
-//	{
-//		while (line[ssl->a] != '"' && (get_next_line(ssl->fd, &line)) > 0)
-//		{
-//			ssl->a = 0;
-//			while (line[ssl->a] && is_whitespace(line[ssl->a]))
-//				(ssl->a)++;
-//			if (line[ssl->a] && line[ssl->a] != '"')
-//				write_error("ERROR COMMENT");
-//			ft_strdel(&line);
-//		}
-//	}
 	(ssl->a)++;
 	while (line[ssl->a] && line[ssl->a] != '"')
 		s[j++] = line[(ssl->a)++];
 	if (line[ssl->a] != '"')
 		j = check_comment3(line, ssl, j, s);
-//	{
-//		s[j++] = '\n';
-//		while ((get_next_line(ssl->fd, &line)) > 0)
-//		{
-//			ssl->a = 0;
-//			while (line[ssl->a] && line[ssl->a] != '"')
-//				s[j++] = line[(ssl->a)++];
-//			if (line[ssl->a] == '"')
-//				break ;
-//			s[j++] = '\n';
-//			ft_strdel(&line);
-//		}
-//	}
 	if (line[ssl->a] != '"')
 		write_error("ERROR NO FINISH COMMENT");
 	if (j > COMMENT_LENGTH)
